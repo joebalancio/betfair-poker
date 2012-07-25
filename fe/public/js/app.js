5386b246c530b29f4cf07c961cb55a053a99f172
Joe Balancio <joseph.balancio@betfair.com>
2012-07-25 03:48:32 +0000

    jlb - Added dependencies
    Added:
    - Twitter Bootstrap for rapid prototyping
    - RequireJS for dependency management
    - jQuery as a general purpose JS framework
    - CreateJS for canvas
    
    Modified the main page and created AMDs for CreateJS

---
+++ fe/public/js/app.js
@@ -0,0 +1,14 @@
+define([
+  'jquery',
+  'backbone',
+  'underscore',
+  'create',
+], function($, Backbone, Underscore, createjs) {
+  console.log(createjs);
+
+  return {
+    initialize: function() {
+      console.log('test');
+    }
+  };
+});
