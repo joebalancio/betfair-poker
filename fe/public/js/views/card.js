67d4645e940a765bbae038403210a007ad7b31bb
Justin Weisberg <Justin.weisberg@betfair.com>
2012-07-27 16:18:09 -0700

    moved ace

---
+++ fe/public/js/views/card.js
@@ -0,0 +1,10 @@
+define(function(require,exports,modules) {
+	var Backbone = require('backbone');
+	var KineticJS = require('kinetic');
+
+	var CardView = Backbone.View.extend({
+		initialize: function() {
+			
+		}
+	})
+})
