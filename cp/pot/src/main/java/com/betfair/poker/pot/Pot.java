4542267410b4c9340614a8e6332fb962e8407d7f
Nahrin Reihaneh <nahrin.reihaneh@betfair.com>
2012-07-27 15:22:45 -0700

    Adding to pot

--- cp/pot/src/main/java/com/betfair/poker/pot/Pot.java
+++ cp/pot/src/main/java/com/betfair/poker/pot/Pot.java
@@ -1,11 +1,35 @@
 package com.betfair.poker.pot;
 
+import java.util.ArrayList;
+import java.util.List;
+import com.betfair.poker.player.Player;
+
 /**
  * A single poker pot.
  */
 public class Pot {
 
-    public Pot() {
-
+	private int bet = 0;
+	private List<Player> players;
+	
+	public Pot() {
+    	players = new ArrayList<Player>();
+    }
+    public Pot(Player player, int bet) {
+    	this.bet = bet;
+    	players = new ArrayList<Player>();
+    	players.add(player);
+    }
+    
+    public void addPlayer(Player player, int bet){
+    	this.bet += bet;
+    	players.add(player);
+    }
+    
+    public  Pot split(List<Player> players, int  partialBet) {
+        Pot pot = new  Pot();
+        pot.bet = partialBet;
+        pot.players = players;
+        return  pot;
     }
 }
