d4c501fdb9349307a604c7a4eb29d2b93f95dc7a
Justin Weisberg <Justin.weisberg@betfair.com>
2012-07-27 14:23:33 -0700

    Removed background style from style, added table to appjs

--- fe/public/js/app.js
+++ fe/public/js/app.js
@@ -3,12 +3,18 @@ define([
   'backbone',
   'underscore',
   'create',
-], function($, Backbone, Underscore, createjs) {
-  console.log(createjs);
+  'views/table'
+], function($, Backbone, Underscore, CreateJS, TableView) {
 
   return {
     initialize: function() {
-      console.log('test');
+      var canvas = $('canvas')[0];
+      var myStage = new CreateJS.Stage(canvas);
+      var tableView = new TableView({stage: myStage});
+      tableView.render();
+      CreateJS.Ticker.addListener({tick: function() {myStage.update()}});
+      CreateJS.Ticker.setFPS(30);
+      myStage.update();
     }
   };
 });
